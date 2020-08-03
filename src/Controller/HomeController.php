5a24f75609944e09bb5a29510e78be1bb8a06a20
manhal abdulraazak <cda19-ama@ccicampus.fr>
2020-08-03 08:57:38 +0200

    add user

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * Home page display
+     *
+     * @Route("/home", name="homepage")
      */
-    public function index()
+    public function index() : Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
